9a3423d152bb8c91bbccba06786abe1072a63e41
Samrudhp <samrudhprakash3084@gmail.com>
2025-02-18 10:23:23 +0530

    map-controller

--- backend/controllers/mapController.js
+++ backend/controllers/mapController.js
@@ -1,1 +1,9 @@
- 
+const { getOptimizedRoute } = require("../utils/api");
+const asyncHandler = require("express-async-handler");
+
+exports.getRouteOptimization = asyncHandler(async(req,res)=>{
+    const {start,end} = req.query;
+    const optimizedRoute = await getOptimizedRoute(start,end);
+    res.json(optimizedRoute);
+    
+})
