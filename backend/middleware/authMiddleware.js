d8ea4f3b687647799ab0939bf59a4ca7b0eb85c7
Samrudhp <samrudhprakash3084@gmail.com>
2025-02-19 17:49:35 +0530

    authmiddleware-added

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,20 +1,24 @@
-
+const User = require('../models/User');
 require('dotenv').config();
 const jwt = require('jsonwebtokens')
-exports.protect = (req,res,next)=>{
-    const token = req.header('Authorization')
-    if(!token) return res.status(401).json({
-        message : 'Access Denied'
-    });
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    }catch(err){
-        res.status(401).json({
-            message: 'Invalid Token'
-        });
 
+
+exports.protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        try {
+            token = req.headers.authorization.split(" ")[1];
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select("-password");
+
+            next();
+        } catch (error) {
+            res.status(401).json({ message: "Not authorized, token failed" });
+        }
+    } else {
+        res.status(401).json({ message: "Not authorized, no token" });
     }
 };
 
--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,7 +1,10 @@
-exports.errorHandler = (err,req,res,next)=>{
-    const statusCode = res.statusCode === 200?500 :res.statusCode;
+// backend/middleware/errorHandler.js
+const errorHandler = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     res.status(statusCode).json({
-        
-    message : err.message
-   });
+        message: err.message,
+        stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
 };
+
+module.exports = { errorHandler };
