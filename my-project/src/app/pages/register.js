2332ae5cd0400e6919dab619010c9bcbf82f1cef
Samrudhp <samrudhprakash3084@gmail.com>
2025-02-21 15:43:56 +0530

    rigisterpage

---
+++ my-project/src/app/pages/register.js
@@ -0,0 +1,9 @@
+import RegisterForm from "../components/Auth/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <RegisterForm />
+    </div>
+  );
+}
