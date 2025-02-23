06d26126079d8152206b328dd91b2bf4c3823370
Samrudhp <samrudhprakash3084@gmail.com>
2025-02-23 19:27:24 +0530

    dashboard

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,24 @@
+import Navbar from "../components/Navbar";
+import Map from "../components/Map";
+import BatteryStatus from "../components/BatteryStatus";
+import TripHistory from "../components/TripHistory";
+import Profile from "../components/Profile";
+
+const Dashboard = () => {
+  return (
+    <div className="flex">
+      <Navbar />
+      <div className="flex-1 ml-64 p-8 bg-gray-100 min-h-screen">
+        <h1 className="text-3xl font-bold mb-8">Dashboard</h1>
+        <div className="space-y-8">
+          <Map />
+          <BatteryStatus />
+          <TripHistory />
+          <Profile />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
